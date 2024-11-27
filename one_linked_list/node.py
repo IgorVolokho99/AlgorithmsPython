48a9248cc3018c5fb9d623c9cad7f8056b0c8795
Igor Volokho <igorvolokho99@gmail.com>
2024-11-27 23:50:43 +0300

    #9 Ruff checker has been added.

--- one_linked_list/node.py
+++ one_linked_list/node.py
@@ -1,7 +1,15 @@
+"""Модуль, содержащий реализацию Node."""
+
 from typing import Any
 
+
 class Node:
-    def __init__(self, value=None):
+    """Класс Node, реализующий в себе логику узлов односвязного списка."""
+
+    def __init__(self, value: Any = None):
+        """Конструктор класса Node.
+
+        :param value: Необязательный параметр value любого типа.
+        """
         self.value = value
         self.next = None
-
--- one_linked_list/one_linked_list.py
+++ one_linked_list/one_linked_list.py
@@ -1,20 +1,33 @@
+"""Модуль, содержащий в себе реализацию односвязного списка."""
+
 from typing import Any
 
 from node import Node
 
 
 class OneLinkedList:
+    """Класс, реализующий односвязный список."""
+
     def __init__(self, node: Node = None):
+        """Конструктор класса OneLinkedList.
+
+        Принимает необязателньый аргумент node, который по умолчанию сохраняется в атрибуты head и tail.
+        :param node: Необязательный аргумент.
+        """
         self.head = node
         self.tail = node
 
     def append(self, data: Any) -> None:
+        """Добавление элемента в односвязный список.
+
+        Метод, принимающий на вход объект любого типа из которого формируется экземпляр класса Node и добавляется
+        в односвянзный список. В зависимости от того пуст список или нет объект может быть добавлен в атрибут head.
+        :param data: Объект любого типа.
+        :return: None
+        """
         node = Node(data)
         if self.head is None:
             self.head = self.tail = node
         else:
             self.tail.next = node
             self.tail = self.tail.next
-
-
-
