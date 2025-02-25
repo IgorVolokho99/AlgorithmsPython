f1c04ef4c83c0a08cbb6d977de7a7a557a9859b8
Igor Volokho <igorvolokho99@gmail.com>
2025-02-25 19:22:30 +0300

    #47 Main structure of two-linked-list.

--- two_linked_list/two_linked_list.py
+++ two_linked_list/two_linked_list.py
@@ -12,3 +12,83 @@ class TwoLinkedList:
         """
         return self.head is None
 
+    def push_back(self):
+        pass
+
+    def pop_back(self):
+        pass
+
+    def push_front(self):
+        pass
+
+    def pop_front(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def find(self):
+        pass
+
+    def get(self):
+        pass
+
+    def set(self):
+        pass
+
+    def size(self):
+        pass
+
+    def reverse(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def extend(self):
+        pass
+
+    def __copy__(self):
+        pass
+
+    def __deepcopy__(self, memodict={}):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __reversed__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __contains__(self, item):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def __bool__(self):
+        pass
