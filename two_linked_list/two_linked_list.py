5dc8b999a8084b66ed2e8a2f07cdda6e0093c0b3
Igor Volokho <igorvolokho99@gmail.com>
2025-02-25 19:16:23 +0300

    #46 Method is_empty has been implemented.

--- two_linked_list/two_linked_list.py
+++ two_linked_list/two_linked_list.py
@@ -2,3 +2,13 @@ class TwoLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def is_empty(self) -> bool:
+        """Метод, проверяющий является ли список пуст.
+
+        Returns:
+            bool: True, если список пуст и False в противном случае.
+
+        """
+        return self.head is None
+
