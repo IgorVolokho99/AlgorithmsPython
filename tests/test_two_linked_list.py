e9fc12e420a8385e8985b360c443a03278b38888
Igor Volokho <igorvolokho99@gmail.com>
2025-05-26 22:25:48 +0300

    #61 Method set(TESTS)

---
+++ tests/test_two_linked_list.py
@@ -0,0 +1,71 @@
+from typing import Any, TypeVar, Type
+
+import pytest
+
+from two_linked_list.two_linked_list import TwoLinkedList
+
+E = TypeVar("E", bound=BaseException)
+
+
+class TestMethodEq:
+    @pytest.mark.parametrize(
+        "initial_list_1, initial_list_2",
+        [
+            ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
+            ([1.0, 2.0, 3.0], [1.0, 2.0, 3.0]),
+            (["a", "b", "c"], ["a", "b", "c"]),
+            (["a", 1, 2.0], ["a", 1, 2.0]),
+            ([[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]),
+            ([], [])
+        ]
+    )
+    def test_equal_lists(self, initial_list_1, initial_list_2):
+        assert TwoLinkedList(initial_list_1) == TwoLinkedList(initial_list_2)
+
+    @pytest.mark.parametrize(
+        "initial_list_1, initial_list_2",
+        [
+            ([1, 2, 3], [1, 2, 4]),
+            ([1, 2, 3], [1, 2, 3.0]),
+            (["a"], [1]),
+            ([1, 2, 3], []),
+            ([], [1, 2, 3])
+        ]
+    )
+    def test_not_equal_lists(self, initial_list_1, initial_list_2):
+        assert TwoLinkedList(initial_list_1) != TwoLinkedList(initial_list_2)
+
+
+class TestSet:
+    # pytest tests/test_two_linked_list.py::TestSet
+    @pytest.mark.parametrize(
+        "two_linked_list, index, value, expected",
+        [
+            (TwoLinkedList([1, 2, 3, 4, 5]), 0, 10, 10),
+            (TwoLinkedList([1, 2, 3, 4, 5]), 4, 10, 10),
+            (TwoLinkedList([1, 2, 3, 4, 5]), 2, 10, 10),
+            (TwoLinkedList([1, 2, 3, 4, 5]), 2, "10", "10"),
+            (TwoLinkedList([1, 2, 3, 4, 5]), 2, True, True)
+        ]
+    )
+    def test_valid_args(self, two_linked_list: TwoLinkedList, index: int, value: Any, expected: Any) -> None:
+        two_linked_list.set(index, value)
+        assert two_linked_list.get(index) == expected
+
+    @pytest.mark.parametrize(
+        "two_linked_list, index, value, expected_exception",
+        [
+            (TwoLinkedList([1, 2, 3, 4, 5]), 5.0, 10, TypeError),
+            (TwoLinkedList([1, 2, 3, 4, 5]), "5.0", 10, TypeError),
+            (TwoLinkedList([1, 2, 3, 4, 5]), True, 10, TypeError),
+            (TwoLinkedList([1, 2, 3, 4, 5]), [], 10, TypeError),
+            (TwoLinkedList([1, 2, 3, 4, 5]), -1, 10, ValueError),
+            (TwoLinkedList([1, 2, 3, 4, 5]), -500, 10, ValueError),
+            (TwoLinkedList([1, 2, 3, 4, 5]), 5, 10, IndexError),
+            (TwoLinkedList([1, 2, 3, 4, 5]), 500, 10, IndexError)
+        ]
+    )
+    def test_invalid_args(self, two_linked_list: TwoLinkedList, index: Any, value: Any,
+                          expected_exception: Type[E] | tuple[Type[E], ...]) -> None:
+        with pytest.raises(expected_exception):
+            two_linked_list.set(index, value)
