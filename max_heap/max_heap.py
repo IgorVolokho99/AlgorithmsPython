9f43cfe05a064873003367d80654933ab704436b
Igor Volokho <igorvolokho99@gmail.com>
2025-07-07 15:05:46 +0300

    #107 MVP of max-binary-heap.

---
+++ max_heap/max_heap.py
@@ -0,0 +1,29 @@
+"""Модуль, содержащий реализацию двоичной кучи(max-binary-heap)."""
+
+
+class Heap:
+    """Реализация двоичной кучи."""
+
+    def __init__(self) -> None:
+        pass
+
+    # def push(self) -> None:
+    #     pass
+    #
+    # def pop(self) -> int:
+    #     pass
+    #
+    # def peek(self) -> int:
+    #     pass
+    #
+    # def is_empty(self) -> bool:
+    #     pass
+    #
+    # def clear(self) -> None:
+    #     pass
+    #
+    # def heapify(self) -> None:
+    #     pass
+    #
+    # def __len__(self) -> int:
+    #     pass
