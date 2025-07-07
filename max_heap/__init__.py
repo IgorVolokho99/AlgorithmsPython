a83221c5d1189e99fd3b2b18ef37a8ef1334b9e0
Igor Volokho <igorvolokho99@gmail.com>
2025-07-07 16:05:23 +0300

    #108 Methods push, pop, peek, is_empty, clear, depth, __leen__, _heapify_up, _heapify_down had been implemented.

--- max_heap/__init__.py
+++ max_heap/__init__.py
@@ -1,1 +1,5 @@
 """Пакет, содержащий модули для реализации двоичной кучи(max-binary-heap)."""
+
+from max_heap.max_heap import Heap
+
+__all__ = ["Heap"]
--- max_heap/max_heap.py
+++ max_heap/max_heap.py
@@ -1,29 +1,119 @@
 """Модуль, содержащий реализацию двоичной кучи(max-binary-heap)."""
+import math
 
 
 class Heap:
     """Реализация двоичной кучи."""
 
     def __init__(self) -> None:
-        pass
+        """Инициализирует кучу пустым массивом."""
+        self._data = []
 
-    # def push(self) -> None:
-    #     pass
-    #
-    # def pop(self) -> int:
-    #     pass
-    #
-    # def peek(self) -> int:
-    #     pass
-    #
-    # def is_empty(self) -> bool:
-    #     pass
-    #
-    # def clear(self) -> None:
-    #     pass
-    #
-    # def heapify(self) -> None:
-    #     pass
-    #
-    # def __len__(self) -> int:
-    #     pass
+    def push(self, number: int) -> None:
+        """Добавляет число в кучу.
+
+        Args:
+            number(int) : Число, которое будет добавлено в кучу.
+
+        """
+        if not (isinstance(number, int) or isinstance(number, float)):
+            raise TypeError("Push-value must be an integer or float.")
+        index = len(self._data)
+        self._data.append(number)
+        self._heapify_up(index)
+
+    def _heapify_up(self, index: int) -> None:
+        """Проводит "хипизацию" кучи, после добавления нового элемента.
+
+        Элемент под указанным индексом поднимается вверх, покаместь не будет выполнено основное свойство кучи.
+
+        Args:
+            index(int) : Позиция с которой нужно провести хипизацию вверх;
+
+        """
+        if (index - 1) // 2 >= 0 and self._data[index] > self._data[(index - 1) // 2]:
+            self._data[index], self._data[(index - 1) // 2] = self._data[(index - 1) // 2], self._data[index]
+            self._heapify_up((index - 1) // 2)
+
+    def pop(self) -> int:
+        """Возвращает и удаляет максимальный элемент кучи.
+
+        Меняет местами первый и последний элемент кучи, запоминает и удаляет максимальный элемент, который попал в конец
+        списка. Проводится хипизация кучи вниз для корневого элемента.
+
+        Returns:
+            int: Максимальный элемент кучи.
+
+        """
+        self._data[0], self._data[-1] = self._data[-1], self._data[0]
+        return_value = self._data.pop()
+        self._heapify_down(0)
+        return return_value
+
+    def _heapify_down(self, index: int) -> None:
+        """Проводит "хипизацию" кучи, после добавления нового элемента.
+
+        Элемент под указанным индексом опускается вниз, покаместь не будет выполнено основное свойство кучи.
+
+        Args:
+            index(int) : Позиция с которой нужно провести хипизацию вверх;
+
+        """
+        while index * 2 + 2 < len(self._data):
+            if self._data[index * 2 + 1] > self._data[index * 2 + 2]:
+                new_index = index * 2 + 1
+            else:
+                new_index = index * 2 + 2
+            if self._data[index] < self._data[new_index]:
+                self._data[index], self._data[new_index] = self._data[new_index], self._data[index]
+                index = new_index
+            else:
+                break
+
+        if index * 2 + 1 < len(self._data):
+            new_index = index * 2 + 1
+            if self._data[index] < self._data[new_index]:
+                self._data[index], self._data[new_index] = self._data[new_index], self._data[index]
+
+    def peek(self) -> int:
+        """Возвращает самое большое число в куче.
+
+        Returns:
+            int: Самое большое число кучи, стоящее на вершине.
+
+        """
+        return self._data[0]
+
+    def is_empty(self) -> bool:
+        """Проводит проверку пустоты кучи.
+
+        Returns:
+            bool: True, если куча пустая и False в противном случае.
+
+        """
+        return True if len(self._data) == 0 else False
+
+    def clear(self) -> None:
+        """Удаляет все узлы в куче."""
+        self._data.clear()
+
+    def depth(self) -> int:
+        """Возвращает глубину кучи.
+
+        Returns:
+            int: Глубина кучи, количество уровней на которые заполненна куча полностью или частично.
+
+        """
+        if len(self._data) == 0:
+            return 0
+
+        return int(math.log2(len(self._data))) + 1
+
+    def __len__(self) -> int:
+        """Возвращает количество узлов кучи.
+
+        Returns:
+            int: Количество узлов кучи.
+
+        """
+        return len(self._data)
